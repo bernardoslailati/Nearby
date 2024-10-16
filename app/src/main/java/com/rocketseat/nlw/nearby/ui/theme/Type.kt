bcdda189bd01068527974a96b54252e663915716
Bernardo Michel Slailati <bernardo-slailati@hotmail.com>
2024-10-16 15:23:46 +0200

    Adicionados recursos de fontes

--- app/src/main/java/com/rocketseat/nlw/nearby/ui/theme/Type.kt
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/theme/Type.kt
@@ -2,33 +2,67 @@ package com.rocketseat.nlw.nearby.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.rocketseat.nlw.nearby.R
 
-// Set of Material typography styles to start with
+val rubikFontFamily = FontFamily(
+    Font(R.font.rubik, FontWeight.Normal),
+    Font(R.font.rubik_medium, FontWeight.Medium),
+    Font(R.font.rubik_bold, FontWeight.Bold),
+    Font(R.font.rubik_semibold, FontWeight.SemiBold)
+)
+
 val Typography = Typography(
+    headlineLarge = TextStyle(
+        fontFamily = rubikFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 1.4.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = rubikFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = rubikFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = rubikFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = rubikFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 1.4.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    bodyMedium = TextStyle(
+        fontFamily = rubikFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 1.4.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = rubikFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 1.4.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = rubikFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = rubikFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
     )
-    */
 )
