455f9e04cd97eaf8e605c71dd0d8c07973d4a486
Bernardo Michel Slailati <bernardo-slailati@hotmail.com>
2024-10-18 15:11:27 +0200

    Tela de splash

---
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/screen/MapScreen.kt
@@ -0,0 +1,9 @@
+package com.rocketseat.nlw.nearby.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MapScreen(modifier: Modifier = Modifier) {
+
+}
