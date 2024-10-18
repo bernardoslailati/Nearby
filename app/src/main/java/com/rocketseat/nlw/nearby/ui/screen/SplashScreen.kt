beb960a915d33c982f16445c30529d4382eaffc3
Bernardo Michel Slailati <bernardo-slailati@hotmail.com>
2024-10-18 15:12:12 +0200

    Criada tela de splash

---
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/screen/SplashScreen.kt
@@ -0,0 +1,35 @@
+package com.rocketseat.nlw.nearby.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.rocketseat.nlw.nearby.R
+import com.rocketseat.nlw.nearby.ui.theme.GreenLight
+
+@Composable
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.background(GreenLight).fillMaxSize()) {
+        Image(
+            modifier = Modifier.align(Alignment.Center),
+            painter = painterResource(R.drawable.img_logo_text),
+            contentDescription = "Imagem Logo e Texto"
+        )
+        Image(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            painter = painterResource(R.drawable.bg_splash_screen),
+            contentDescription = "Imagem de Fundo"
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    SplashScreen()
+}
