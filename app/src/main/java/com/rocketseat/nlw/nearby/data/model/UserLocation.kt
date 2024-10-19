8adaf312724296d9043695f8b091ceec1d5a9a5e
Bernardo Michel Slailati <bernardo-slailati@hotmail.com>
2024-10-19 11:22:41 +0200

    Adicionada estrutura de dados de localização do usuário

---
+++ app/src/main/java/com/rocketseat/nlw/nearby/data/model/UserLocation.kt
@@ -0,0 +1,6 @@
+package com.rocketseat.nlw.nearby.data.model
+
+data class UserLocation(
+    val latitude: Double,
+    val longitude: Double
+)
---
+++ app/src/main/java/com/rocketseat/nlw/nearby/data/model/mock/MockUserLocation.kt
@@ -0,0 +1,8 @@
+package com.rocketseat.nlw.nearby.data.model.mock
+
+import com.rocketseat.nlw.nearby.data.model.UserLocation
+
+val mockUserLocation = UserLocation(
+    latitude = -23.561187293883442,
+    longitude =-46.656451388116494,
+)
