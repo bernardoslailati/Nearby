949a43d3af206399f54850ea9149d2e0c1c753d5
Bernardo Michel Slailati <bernardo-slailati@hotmail.com>
2024-11-27 07:53:50 -0300

    Conteúdos da aula 03

--- app/src/main/java/com/rocketseat/nlw/nearby/MainActivity.kt
+++ app/src/main/java/com/rocketseat/nlw/nearby/MainActivity.kt
@@ -4,25 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.rocketseat.nlw.nearby.data.model.Market
-import com.rocketseat.nlw.nearby.ui.screen.HomeScreen
-import com.rocketseat.nlw.nearby.ui.screen.MarketDetailsScreen
-import com.rocketseat.nlw.nearby.ui.screen.SplashScreen
-import com.rocketseat.nlw.nearby.ui.screen.WelcomeScreen
-import com.rocketseat.nlw.nearby.ui.screen.route.Home
-import com.rocketseat.nlw.nearby.ui.screen.route.Splash
-import com.rocketseat.nlw.nearby.ui.screen.route.Welcome
+import com.rocketseat.nlw.nearby.ui.screen.home.HomeScreen
+import com.rocketseat.nlw.nearby.ui.screen.home.HomeViewModel
+import com.rocketseat.nlw.nearby.ui.screen.market_details.MarketDetailsScreen
+import com.rocketseat.nlw.nearby.ui.screen.splash.SplashScreen
+import com.rocketseat.nlw.nearby.ui.screen.welcome.WelcomeScreen
+import com.rocketseat.nlw.nearby.ui.route.Home
+import com.rocketseat.nlw.nearby.ui.route.QRCodeScanner
+import com.rocketseat.nlw.nearby.ui.route.Splash
+import com.rocketseat.nlw.nearby.ui.route.Welcome
+import com.rocketseat.nlw.nearby.ui.screen.market_details.MarketDetailsUiEvent
+import com.rocketseat.nlw.nearby.ui.screen.market_details.MarketDetailsViewModel
+import com.rocketseat.nlw.nearby.ui.screen.qrcode_scanner.QRCodeScannerScreen
 import com.rocketseat.nlw.nearby.ui.theme.NearbyTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +33,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             NearbyTheme {
                 val navController = rememberNavController()
+
+                val homeViewModel by viewModels<HomeViewModel>()
+                val homeUiState by homeViewModel.uiState.collectAsStateWithLifecycle()
+
+                val marketDetailsViewModel by viewModels<MarketDetailsViewModel>()
+                val marketDetailsUiState by marketDetailsViewModel.uiState.collectAsStateWithLifecycle()
+
                 NavHost(
                     navController = navController,
                     startDestination = Splash
@@ -51,16 +59,38 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                     composable<Home> {
-                        HomeScreen(onNavigateToMarketDetails = { selectedMarket ->
-                            navController.navigate(selectedMarket)
-                        })
+                        HomeScreen(
+                            onNavigateToMarketDetails = { selectedMarket ->
+                                navController.navigate(selectedMarket)
+                            },
+                            uiState = homeUiState,
+                            onEvent = homeViewModel::onEvent
+                        )
                     }
                     composable<Market> {
                         val selectedMarket = it.toRoute<Market>()
 
                         MarketDetailsScreen(
                             market = selectedMarket,
+                            uiState = marketDetailsUiState,
+                            onEvent = marketDetailsViewModel::onEvent,
                             onNavigateBack = {
+                                navController.popBackStack()
+                            },
+                            onNavigateToQRCodeScanner = {
+                                navController.navigate(QRCodeScanner)
+                            }
+                        )
+                    }
+                    composable<QRCodeScanner> {
+                        QRCodeScannerScreen(
+                            onCompletedScan = { qrCodeContent ->
+                                if (qrCodeContent.isNotEmpty())
+                                    marketDetailsViewModel.onEvent(
+                                        MarketDetailsUiEvent.OnFetchCoupon(
+                                            qrCodeContent = qrCodeContent
+                                        )
+                                    )
                                 navController.popBackStack()
                             }
                         )
--- app/src/main/java/com/rocketseat/nlw/nearby/ui/component/market/NearbyMarketCard.kt
+++ app/src/main/java/com/rocketseat/nlw/nearby/ui/component/market/NearbyMarketCard.kt
@@ -1,5 +1,6 @@
 package com.rocketseat.nlw.nearby.ui.component.market
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -23,11 +24,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.imageLoader
+import coil3.request.ImageRequest
+import coil3.util.DebugLogger
 import com.rocketseat.nlw.nearby.R
 import com.rocketseat.nlw.nearby.data.model.Market
 import com.rocketseat.nlw.nearby.ui.theme.Gray100
@@ -43,6 +49,10 @@ fun NearbyMarketCard(
     market: Market,
     onClick: (Market) -> Unit
 ) {
+    val imageLoader = LocalContext.current.imageLoader.newBuilder()
+        .logger(DebugLogger())
+        .build()
+
     Card(
         modifier = modifier
             .clip(RoundedCornerShape(12.dp))
@@ -60,14 +70,15 @@ fun NearbyMarketCard(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Image(
+            AsyncImage(
                 modifier = Modifier
                     .clip(RoundedCornerShape(12.dp))
                     .fillMaxWidth(0.3f)
                     .height(IntrinsicSize.Min)
                     .aspectRatio(ratio = 1f, matchHeightConstraintsFirst = true),
                 contentScale = ContentScale.Crop,
-                painter = painterResource(R.drawable.img_burger), // TODO: Substituir pela imagem de market.cover
+                imageLoader = imageLoader,
+                model = market.cover,
                 contentDescription = "Imagem do Estabelecimento"
             )
             Column {
